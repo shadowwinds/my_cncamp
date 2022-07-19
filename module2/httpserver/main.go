6a458fd866ac123862118eba4502b2006c86e021
chenghaotian <htcheng@isoftstone.com>
2022-07-19 10:04:23 +0800

    module2

---
+++ module2/httpserver/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+)
+
+const VERSION_KEY = "VERSION "
+
+func mirrorHandler(writer http.ResponseWriter, request *http.Request) {
+	respHeader := writer.Header()
+	for k, v := range request.Header {
+		for _, vv := range v {
+			respHeader.Add(k, vv)
+		}
+	}
+	respHeader.Set(VERSION_KEY, os.Getenv(VERSION_KEY))
+}
+
+func healthHandler(writer http.ResponseWriter, _ *http.Request) {
+	_, err := writer.Write([]byte("200"))
+	ErrorHandler(err)
+}
+
+func ErrorHandler(write2 error) {
+	if write2 != nil {
+		log.Fatal(write2)
+	}
+}
+
+// ResponseLoggingHandler https://stackoverflow.com/questions/29319783/logging-responses-to-incoming-http-requests-inside-http-handlefunc
+func ResponseLoggingHandler(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// switch out response writer for a recorder
+		// for all subsequent handlers
+		c := httptest.NewRecorder()
+		next(c, r)
+
+		// copy everything from response recorder
+		// to actual response writer
+		for k, v := range c.Header() {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(c.Code)
+		_, err := c.Body.WriteTo(w)
+		ErrorHandler(err)
+		log.Printf("%s %s %s %s %d", r.RemoteAddr, r.Method, r.URL, r.Proto, c.Code)
+	}
+}
+
+func main() {
+	setEnvErr := os.Setenv(VERSION_KEY, "1.0.0")
+	ErrorHandler(setEnvErr)
+
+	port := "8080"
+	log.Println("Listening on :" + port)
+	http.HandleFunc("/health", ResponseLoggingHandler(healthHandler))
+	http.HandleFunc("/", ResponseLoggingHandler(mirrorHandler))
+	err := http.ListenAndServe(":"+port, nil)
+	ErrorHandler(err)
+}
